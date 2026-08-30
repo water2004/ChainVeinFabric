08fe197bff59755048ea4ba4db428bcc58bc5917
GBwater <gbwater@outlook.com>
2026-08-30 17:51:24 +0800

    test: cover client configuration and hotkeys

--- src/gametest/java/org/edtp/chainveinfabric/gametest/mixin/ChainVeinServerCapabilityMixin.java
+++ src/gametest/java/org/edtp/chainveinfabric/gametest/mixin/ChainVeinServerCapabilityMixin.java
@@ -1,16 +1,19 @@
 package org.edtp.chainveinfabric.gametest.mixin;
 
-import org.edtp.chainveinfabric.client.api.ChainVeinClientApi;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /** Simulates a server that does not advertise ChainVein's custom payload. */
-@Mixin(value = ChainVeinClientApi.class, remap = false)
+@Mixin(value = ClientPlayNetworking.class, remap = false)
 public abstract class ChainVeinServerCapabilityMixin {
-    @Inject(method = "canUseServerProtocol", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "canSend(Lnet/minecraft/resources/Identifier;)Z",
+            at = @At("HEAD"), cancellable = true)
     private static void chainVeinGameTest$forceServerAbsent(
+            Identifier channel,
             CallbackInfoReturnable<Boolean> cir) {
         if (!Boolean.parseBoolean(System.getProperty(
                 "chainveinfabric.gametest.serverInstalled", "true"))) {
